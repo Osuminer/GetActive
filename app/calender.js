29ffda66bd6c96c6912cbc91b4855b570d7f31f1
osuminer <connorstaschiak@gmail.com>
2023-07-26 11:18:26 -0400

    Added empty pages

---
+++ app/calender.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Alert, Button, SafeAreaView, ScrollView, View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import ScreenHeaderBtn from "../components/ScreenHeaderBtn/ScreenHeaderBtn";
+
+
+const Calender = () => {
+  const router = useRouter()
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+
+      {/* Header: Profile icon and Home text */}
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn 
+              iconUrl={icons.backArrow} 
+              dimension='70%' 
+              handlePress={() => {
+                router.push()
+              }}/>
+              
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn 
+              iconUrl={icons.ellipsis} 
+              dimension='70%' 
+              handlePress={() => {
+                router.push()
+              }}/>
+          ),
+          headerTitle: "",
+        }}/>
+
+
+
+
+
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default Calender;
---
+++ app/login.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Alert, Button, SafeAreaView, ScrollView, View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import ScreenHeaderBtn from "../components/ScreenHeaderBtn/ScreenHeaderBtn";
+
+
+const Login = () => {
+  const router = useRouter()
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+
+
+
+
+    </SafeAreaView>
+  );
+};
+
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default Login;
