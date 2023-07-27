9bc212f982dd0e6d2e08bde6b43ed6744f12ab16
Zachary Brown <brownz4@mail.uc.edu>
2023-07-26 23:48:43 -0400

    post request, need to actually get data from text inputs

--- app/addWorkout.js
+++ app/addWorkout.js
@@ -7,12 +7,48 @@ import ScreenHeaderBtn from "../components/ScreenHeaderBtn/ScreenHeaderBtn";
 import Workout from "../components/Workout/Workout";
 import Exercise from "../components/Workout/Exercise";
 import ExerciseInput from "../components/Workout/ExerciseInput";
+import { Button } from "react-native-web";
+import config from '../config';
 
 
 const AddWorkout = () => {
   const router = useRouter()
-  let workout = new Workout()
+  const [ workoutTitle, setWorkoutTitle ] = useState('');
+  const [ exercises, setExercises] = useState([]);
+  const [ exerciseTitle, setExerciseTitle] = useState('New Workout');
+  const [ sets, setSets ] = useState(0);
+  const [ reps, setReps] = useState(0)
 
+  const [ workout, setWorkout ] = useState()
+
+  const sendPostRequest = async ( workout ) => {
+    var url = config.baseURL + '/workouts'
+    var response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "title": "Insane in the membrane day",
+        "exercises": [{
+          "title": "read book",
+          "sets": 10,
+          "reps": 8
+        },
+        {
+          "title": "read math book",
+          "sets": 10,
+          "reps": 8
+        },
+        {
+          "title": "read english book",
+          "sets": 10,
+          "reps": 8
+        }
+        ]
+      })
+    });
+    console.log(response);
+  }
+  
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
@@ -28,7 +64,10 @@ const AddWorkout = () => {
                 }}/>
           ),
           headerRight: () => (
-            <ScreenHeaderBtn iconUrl={icons.checkmark} dimension='50%' />
+            <ScreenHeaderBtn handlePress={() => {
+              setWorkout(new Workout(0, workoutTitle, exercises))
+              sendPostRequest( workout )
+            }} iconUrl={icons.checkmark} dimension='50%' />
           ),
           headerTitle: "",
         }}
@@ -37,7 +76,8 @@ const AddWorkout = () => {
       <View style={styles.view}>
         <TextInput 
           style={styles.titleText}
-          defaultValue="New Workout"
+          value={workoutTitle}
+          onChange={setWorkoutTitle}
           maxLength={20}/>
         
         <View style={styles.buttonView}>
@@ -53,13 +93,26 @@ const AddWorkout = () => {
             justifyContent: "center",
             alignItems: "center",
             }}>
-
-            <ExerciseInput />
-            <ExerciseInput />
-            <ExerciseInput />
-            <ExerciseInput />
-            <ExerciseInput />
-
+            <View style={{flex: 1, flexDirection: 'row'}}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Title"
+                placeholderTextColor={'#444444'}
+                value={exerciseTitle}
+                onChange={setExerciseTitle} />
+              <TextInput
+                style={styles.textInput}
+                placeholder="Sets"
+                placeholderTextColor={'#444444'}
+                value={sets}
+                onChange={setSets} />
+              <TextInput
+                style={styles.textInput}
+                placeholder="Reps"
+                placeholderTextColor={'#444444'}
+                value={reps}
+                onChange={setReps} />
+		      </View>
         </ScrollView>
       </View>
     </SafeAreaView>
@@ -125,6 +178,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  textInput: {
+		padding: 15,
+		  margin: 10,
+		  borderRadius: SIZES.small / 1.25,
+		  backgroundColor: '#eaeaea',
+	  },
 });
 
 export default AddWorkout;
